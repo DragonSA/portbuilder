02908d7f145d9a0105a6851f6d66d17ead05aea7
David Naylor <naylor.b.david@gmail.com>
2011-02-14 05:35:38 +0000

    - add port cache

--- pyport/port/__init__.py
+++ pyport/port/__init__.py
@@ -1,1 +1,34 @@
- 
+"""FreeBSD Ports."""
+
+class PortCache(object):
+  """Caches created ports."""
+
+  def __init__(self):
+    """Initialise port cache."""
+    self.ports = {}
+    self.waiters = {}
+
+  def get_port(self, origin, callback):
+    """Get a port and callback with it."""
+    if origin in self.ports:
+      from ..event import post_event
+      post_event(callback, self.ports[origin])
+    else:
+      from .mk import attr
+      self.waiters.setdefault(origin, []).append(callback)
+      attr(origin, self._attr)
+
+  def _attr(self, origin, attr):
+    """Use attr to create a port."""
+    from ..event import post_event
+    from .port import Port
+
+    waiters = self.waiters.pop(origin)
+    port = None if attr is None else Port(origin, attr)
+    self.ports[origin] = port
+    for callback in waiters:
+      post_event(callback, port)
+
+_cache = PortCache()
+
+get_port = _cache.get_port
