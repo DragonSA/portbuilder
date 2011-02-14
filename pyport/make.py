f5227897a64d7056d80caccbecb4b9cb7fbbe75a
David Naylor <naylor.b.david@gmail.com>
2011-02-14 05:35:31 +0000

    - add make target builder

---
+++ pyport/make.py
@@ -0,0 +1,44 @@
+"""Make targets."""
+
+__all__ = ["SUCCESS", "make_target"]
+
+SUCCESS = 0
+
+def kwargs2str(kwargs):
+  for key, value in kwargs.items():
+    if value is True:
+      yield "-D%s" % key
+    else:
+      yield "%s=%s" % (key, value)
+
+def make_target(callback, port, targets, pipe=None, **kwargs):
+  """Build a make target and call a function when finished."""
+  from os.path import make
+  from subprocess import PIPE, STDOUT, Popen
+  from .subprocess import add_popen
+
+  if type(targets) is str:
+    targets = (targets,)
+  if type(targets) != tuple:
+    targets = tuple(targets)
+
+  PORTSDIR = "/usr/ports"
+  args = ("make", "-C", join(PORTSDIR, port.origin)) + targets
+  args += tuple(kwargs2str(kwargs))
+
+  if pipe is True:
+    stdin, stdout, stderr = PIPE, PIPE, STDOUT
+  elif pipe is False:
+    stdin, stdout, stderr = None, None, None
+  else:
+    stdin = PIPE
+    stdout = open(port.log_file, 'a')
+    stderr = stdout
+
+  make = Popen(args, stdin=stdin, stdout=stdout, stderr=stderr, close_fds=True)
+  if stdin is not None:
+    make.stdin.close()
+
+  add_popen(make, callback)
+
+  return make
