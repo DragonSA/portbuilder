5449523ff8f2fa510a243e5e3be8118247afaeb9
David Naylor <naylor.b.david@gmail.com>
2011-02-14 05:35:30 +0000

    - add subprocess monitor

---
+++ pyport/subprocess.py
@@ -0,0 +1,50 @@
+"""Monitor subprocesses for completion (via signal(3))"""
+
+__all__ = ["active_popen", "add_popen"]
+
+class ChildrenMonitor(object):
+  """Monitor subprocesses."""
+
+  def __init__(self):
+    """Initialise subprocess monitor."""
+    from signal import signal, SIGCHLD
+
+    self._pid_map = {}
+    signal(SIGCHLD, self._signal)
+
+  def __len__(self):
+    """Number of active subprocesses."""
+    return len(self._pid_map)
+
+  def add_popen(self, popen, callback):
+    """Add a popen instance to be monitored, with callback function."""
+    self._pid_map[popen.pid] = (popen, callback)
+
+  def _signal(self, signum, frame):
+    """Handle a signal from child process."""
+    from os import wait
+    from .event import post_event
+
+    pid, status = wait()
+
+    if status & 0xff:
+      # If low byte set then process exited due to signal
+      status = -(status & 0xff)
+    else:
+      # Else high byte contains exit status
+      status = status >> 8
+    post_event(self._process_signal, pid, status)
+
+  def _process_signal(self, pid, status):
+    """Update the subprocess object and dispatch the callback."""
+    from .event import post_event
+
+    popen, callback = self._pid_map.pop(pid)
+    popen.returncode = status
+
+    post_event(callback, popen)
+
+_monitor = ChildrenMonitor()
+
+active_popen = _monitor.__len__
+add_popen = _monitor.add_popen
