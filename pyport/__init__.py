2c84f2a9c6048834049183341ff3fecd9dd2030d
David Naylor <naylor.b.david@gmail.com>
2011-02-14 05:35:48 +0000

    - add all queues   - base queue load on cpu count

--- pyport/__init__.py
+++ pyport/__init__.py
@@ -1,8 +1,15 @@
 """FreeBSD port building infrastructure."""
 
-from .queue import QueueManager as _QM
+from multiprocessing import cpu_count as _cpu_count
+from .queue import QueueManager as _QueueManager
 
 __all__ = ["attr_queue", "config_queue"]
 
-attr_queue = _QM(8)
-config_queue = _QM(4)
+_cpus = _cpu_count()
+
+attr_queue = _QueueManager(_cpus * 2)
+config_queue = _QueueManager(max(2, _cpus * 2))
+checksum_queue = _QueueManager(1)
+fetch_queue = _QueueManager(1)
+build_queue = _QueueManager(_cpus * 2)
+install_queue = _QueueManager(1)
