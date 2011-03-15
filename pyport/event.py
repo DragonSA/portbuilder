559e4eb66eb2278c5d925fe9b16aae30f063d68b
David Naylor <naylor.b.david@gmail.com>
2011-03-15 18:19:18 +0000

    - add support for alarm callbacks

--- pyport/event.py
+++ pyport/event.py
@@ -13,9 +13,15 @@ class EventManager(object):
     from collections import deque
 
     self._events = deque()
+    self._alarms = []
 
   def __len__(self):
     return len(self._events)
+
+  def alarm(self, callback, interval):
+    """Add a function for period callback."""
+    from time import time
+    self._alarms.append([callback, time() + interval])
 
   def post_event(self, func, *args, **kwargs):
     """Add an event to be called asynchroniously."""
@@ -27,15 +33,52 @@ class EventManager(object):
 
   def run(self):
     """Run the currently queued events."""
-    while len(self._events):
-      func, args, kwargs = self._events.popleft()
+    from time import time, sleep
+    from .subprocess import active_popen
+
+    while True:
+      while len(self._events):
+        self._alarm
+        func, args, kwargs = self._events.popleft()
+        try:
+          func(*args, **kwargs)
+        except BaseException:
+          raise
+
+      if not active_popen():
+        break
+
       try:
-        func(*args, **kwargs)
-      except BaseException:
-        raise
+        sleep_intr = min(i[1] for i in self._alarms) - time()
+      except ValueError:
+        sleep_intr = 1
+
+      if sleep_intr <= 0:
+        self._alarm()
+      else:
+        sleep(min(sleep_intr, 1))
+
+    self._alarm(True)
+
+  def _alarm(self, end=False):
+    """Run all outstanding alarms."""
+    from time import time
+
+    now = time()
+    for item in reversed(self._alarms):
+      if item[1] <= time or end:
+        try:
+          trigger = item[0](end)
+        except BaseException:
+          raise
+        if trigger:
+          item[1] = now + trigger
+        else:
+          self._alarms.remove(item)
 
 _manager = EventManager()
 
+alarm = _manager.alarm
 pending_events = _manager.__len__
 post_event = _manager.post_event
 run = _manager.run
