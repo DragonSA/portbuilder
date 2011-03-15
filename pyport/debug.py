71ae865e33180c28735623498861793157f4783c
David Naylor <naylor.b.david@gmail.com>
2011-03-15 18:20:02 +0000

    - add traceback getter

---
+++ pyport/debug.py
@@ -0,0 +1,13 @@
+"""Support functions for debuging."""
+
+__all__ = ["get_tb"]
+
+def get_tb(offset=0):
+  """Get the current traceback, excluding the top `offset` frames."""
+  from .env import flags
+
+  if flags["debug"]:
+    from traceback import extract_stack
+    return extract_stack()[:-1 - offset]
+  else:
+    return None
