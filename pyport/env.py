d489b03139c3701d9acb9a747a5b601e89b958c5
David Naylor <naylor.b.david@gmail.com>
2011-03-15 18:19:38 +0000

    - add config and package to library flags

--- pyport/env.py
+++ pyport/env.py
@@ -10,14 +10,16 @@ PKG_DBDIR = "/var/db/pkg"
 PORTSDIR = "/usr/ports"
 
 env = {
-  "PKG_DBDIR" : PKG_DBDIR,
-  "PORTSDIR"  : PORTSDIR
+  "PKG_DBDIR" : PKG_DBDIR,  # Package database directory
+  "PORTSDIR"  : PORTSDIR,   # Ports directory
 }
 
 flags = {
-  "debug" : False,        # Print extra debug messages
-  "no_op" : False,        # Do nothing
-  "no_op_print" : False,  # Print commands that would have been executed
+  "config"      : "changed"  # Configure ports based on criteria
+  "debug"       : False,     # Print extra debug messages
+  "no_op"       : False,     # Do nothing
+  "no_op_print" : False,     # Print commands that would have been executed
+  "package"     : False,     # Package installed ports
 }
 
 env_master = {}
