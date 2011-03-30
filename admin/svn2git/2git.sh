003a43676f2dc37e21a6319c509b6549109f2485
David Naylor <naylor.b.david@gmail.com>
2011-03-30 18:49:28 +0000

    - update script to remove svk extra messages

--- admin/svn2git/2git.sh
+++ admin/svn2git/2git.sh
@@ -5,8 +5,29 @@ REPO=${REPO:-/home/DragonSA/archive/projects/pypkg/}
 
 cd `dirname $0`
 
-svn2git --identity-map=authors.txt --rules=rules.txt --stats ${REPO}
+# Convert to git
+if svn2git --identity-map=authors.txt --rules=rules.txt --stats ${REPO}
+then
+  rm log-portbuilder
+else
+  exit 1
+fi
 
+(cd portbuilder;
+	git filter-branch --tag-name-filter cat --msg-filter "sed -E '/^ r[0-9]+@.*:  .* | [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} \+[0-9]{4}$/D'" -- --all;
+	git filter-branch --tag-name-filter cat --msg-filter 'tee `mktemp /tmp/log.XXXXX`';
+	git for-each-ref --format='%(refname)' refs/original/ refs/remotes/svn/ | while read ref;
+	do
+		git update-ref -d "$ref";
+	done;
+	git reflog expire --all --expire=now;
+	git gc --aggressive;
+	git prune;
+	git fsck --full)
+
+# Make bare git repository a normal git repository
+# To skip uncomment next line
+# exit 0
 (cd portbuilder;
 	git config core.bare false;
 	mkdir .git;
