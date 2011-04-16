d49312c2a4f48624c48773d15bfbdf46a1677b3b
David Naylor <naylor.b.david@gmail.com>
2011-04-16 13:26:52 +0200

    Fix port
    Use correct variable for -devel and correctly set the COMMENT line

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='portbuilder',
       version="0.1.2",
-      description="Concurrent FreeBSD port builder.",
+      description="Concurrent FreeBSD port builder",
       author="David Naylor",
       author_email="naylor.b.david@gmail.com",
       url="http://github.com/DragonSA/portbuilder/",
