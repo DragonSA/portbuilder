c3526f51afc23a4c672e86a850c4f49d094a2482
David Naylor <naylor.b.david@gmail.com>
2011-09-19 20:25:31 +0200

    Add ability to iterate over all currently loaded ports.
    This function provides a snapshot of ports and does not provide support to
    query ports that are still loading.

--- libpb/port/__init__.py
+++ libpb/port/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import
 
-__all__ = ["get_port"]
+__all__ = ["get_port", "get_ports", "ports"]
 
 
 class PortCache(object):
@@ -37,6 +37,9 @@ class PortCache(object):
                 attr(origin).connect(self._attr)
                 return signal
 
+    def __iter__(self):
+        return self._ports.values()
+
     def _attr(self, origin, attr):
         """Use attr to create a port."""
         from .port import Port
@@ -54,3 +57,4 @@ _cache = PortCache()
 
 ports = _cache.__len__
 get_port = _cache.get_port
+get_ports = _cache.__iter__
