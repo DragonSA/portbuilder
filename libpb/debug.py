d38a555e32bd51de412ee56e4a668fbc07319cd4
David Naylor <naylor.b.david@gmail.com>
2012-04-17 12:12:50 +0200

    Extend the debug module to display trace messages and for functions debug, info.
    This module needs to be refactored to properly support message levels and to
    consolidate the traceback printing routines.

--- libpb/debug.py
+++ libpb/debug.py
@@ -6,6 +6,8 @@ import datetime
 import os
 import traceback
 
+from libpb import env
+
 __all__ = ["error", "exception", "get_tb"]
 
 
@@ -20,13 +22,29 @@ def get_tb(offset=0):
         return None
 
 
-def error(func, msg):
+def error(func, msg, trace=False):
     """Report an error to the general logfile"""
     from .env import flags
 
     fullmsg = "%s %s> %s\n" % (datetime.datetime.now(), func, "\n  ".join(msg))
 
     open(os.path.join(flags["log_dir"], flags["log_file"]), "a").write(fullmsg)
+
+    if trace and env.flags["debug"]:
+        from .event import traceback as event_traceback
+        from .env import flags
+
+        log = open(os.path.join(flags["log_dir"], flags["log_file"]), "a")
+        msg = "  "
+        for tb, name in event_traceback() + [(get_tb(), "message")]:
+            msg += "Traceback from %s (most recent call last):\n" % name
+            msg += "%s\n" % "".join(traceback.format_list(tb))
+        log.write(msg.replace("\n", "\n  ")[:-2])
+        log.close()
+
+
+info = error
+debug = error
 
 
 def exception():
@@ -35,11 +53,12 @@ def exception():
     from .env import flags
 
     log = open(os.path.join(flags["log_dir"], flags["log_file"]), "a")
-    log.write("%s> EXCEPTION\n  " % datetime.datetime.now())
-    msg = ""
+    log.write("%s> EXCEPTION\n" % datetime.datetime.now())
+    msg = "  "
     for tb, name in event_traceback():
         msg += "Traceback from %s (most recent call last):\n" % name
         msg += "%s\n" % "".join(traceback.format_list(tb))
     msg += traceback.format_exc()
-    log.write(msg.replace("\n", "\n  "))
+    msg += "\n"
+    log.write(msg.replace("\n", "\n  ")[:-2])
     log.close()
