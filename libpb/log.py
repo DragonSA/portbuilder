8f34fe02d3fb011e671ca09af8c821907ff08943
David Naylor <naylor.b.david@gmail.com>
2012-05-27 20:42:24 +0200

    Do not print the stack above event.py:run() and increase log time accuracy.

--- libpb/log.py
+++ libpb/log.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import, with_statement
 
 import os
+import sys
 import time
 import traceback
 
@@ -26,6 +27,11 @@ def format_tb(tb, name):
     if tb is None:
         return ""
     msg = "Traceback from %s (most recent call last):\n" % name
+    for i in range(len(tb)):
+        stack = tb[i]
+        if stack[0].endswith("libpb/event.py") and stack[2] == "run":
+            tb = tb[i + 1:]
+            break
     msg += "%s\n" % "".join(traceback.format_list(tb))
     return msg
 
@@ -44,7 +50,7 @@ def debug(func, msg):
     """Log a debug message to log file (only if in debug mode)."""
     if env.flags["debug"]:
         msg = msg.replace("\n", "n  ")
-        msg = "[%10.3f] (D) %s> %s\n" % (offset_time(), func, msg)
+        msg = "[%11.4f] (D) %s> %s\n" % (offset_time(), func, msg)
         with open(logfile(), "a") as log:
             log.write(msg)
 
@@ -52,7 +58,7 @@ def debug(func, msg):
 def error(func, msg, trace=False):
     """Report an error to the general logfile"""
     msg = msg.replace("\n", "n  ")
-    fullmsg = "[%10.3f] (E) %s> %s\n" % (offset_time(), func, msg)
+    fullmsg = "[%11.4f] (E) %s> %s\n" % (offset_time(), func, msg)
     if trace and env.flags["debug"]:
         from libpb import event
         msg = "  "
@@ -67,10 +73,13 @@ def error(func, msg, trace=False):
 def exception():
     """Report an exception to the general logfile"""
     from libpb import event
-    msg = "  "
+    msg = ""
     msg += "".join(format_tb(tb, name) for tb, name in event.traceback())
-    msg += traceback.format_exc()
+    exc_type, exc_value, exc_tb = sys.exc_info()
+    msg += format_tb(traceback.extract_tb(exc_tb), "exception")[:-1]
+    msg += "%s: %s" % (exc_type.__name__, exc_value)
     msg += "\n"
     with open(logfile(), "a") as log:
-        log.write("[%10.3f] (EXCEPTION)\n" % (offset_time()))
+        log.write("[%10.3f] (EXCEPTION)\n  " % (offset_time()))
         log.write(msg.replace("\n", "\n  ")[:-2])
+    return msg
